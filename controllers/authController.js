862a2d5246ba3ef725c4c74b9d7296f03c802d38
Azamshaik7 <azamshari68@gmail.com>
2024-12-31 16:43:38 +0530

    ss

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,35 +35,67 @@ const signup = async (req, res) => {
 };
 
 // Login controller
+// const login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Find the user by email
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(400).json({ message: 'Invalid email or password' });
+//     }
+
+//     // Compare the password with the hashed password
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+//     if (!isPasswordValid) {
+//       return res.status(400).json({ message: 'Invalid email or password' });
+//     }
+
+//     // Generate a JWT token
+//     const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+
+//     // Send response with the token
+//     return res.status(200).json({
+//       message: 'Login successful',
+//       token, // Send the token to the client
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// };
+
+
+
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Find the user by email
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-    // Compare the password with the hashed password
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-    // Generate a JWT token
     const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
 
-    // Send response with the token
     return res.status(200).json({
       message: 'Login successful',
-      token, // Send the token to the client
+      token,
+      name: user.name, // Include the user's name in the response
     });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 };
+
+
+
 
 // Book appointment controller
 const bookAppointment = async (req, res) => {
